591564306bc513bf0b004a2d19a804f93e78c6a3
Opilane TTHK <opilane@tthk.ee>
2022-02-18 10:52:43 +0200

    yes

---
+++ SQLiteCountries/Database.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Text;
+
+namespace SQLiteCountries
+{
+    class Database
+    {
+        public SQLiteConnection myConnection;
+
+        public Database()
+        {
+            myConnection = new SQLiteConnection("Data source=countries.db");
+        }
+
+        public void OpenConnection()
+        {
+            if(myConnection.State != System.Data.ConnectionState.Open)
+            {
+                myConnection.Open();
+            }
+        }
+        public void CloseConnection()
+        {
+            if (myConnection.State != System.Data.ConnectionState.Closed)
+            {
+                myConnection.Close();
+            }
+        }
+    }
+}
---
+++ SQLiteCountries/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace SQLiteCountries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadCountries();
+        }
+
+        private static void ReadCountries()
+        {
+            Database databaseObj = new Database();
+            string query = "SELECT Country.countryName as Country, Capital.capitalname as Capital FROM Capital join Country on Capital.countryid = Country.rowid";
+            SQLiteCommand myCommand = new SQLiteCommand(query, databaseObj.myConnection);
+            databaseObj.OpenConnection();
+
+            SQLiteDataReader data = myCommand.ExecuteReader();
+
+            if (data.HasRows)
+            {
+                while (data.Read())
+                {
+                    Console.WriteLine($"Country: {data["country"]}. Capital: {data["Capital"]}");
+                }
+            }
+
+            databaseObj.CloseConnection();
+        }
+    }
+}
